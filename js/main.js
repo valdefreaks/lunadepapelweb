da55367c50bafa60cc7751c7aa05d1537191626f
valdefreaks <valdefreaks92@gmail.com>
2019-03-22 20:54:50 -0600

    desde servidor

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function(){
+	"use strict";
+	document.addEventListener('DOMContentLoaded', function(){
+
+
+
+		console.log('Hola mundo');
+
+		// CATALOGOS
+
+		// Divs de submenu
+
+		var divProductos = document.getElementById('productos');
+		var divClientes = document.getElementById('productos');
+		var divProveedores = document.getElementById('productos');
+
+
+
+		console.log(divProductos);
+
+
+	}); //DOM CONTENT LOADED
+})();
